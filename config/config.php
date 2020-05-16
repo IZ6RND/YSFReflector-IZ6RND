a04e879aeb0a8a4e2758126dfb6326951fb5cdb1
IZ6RND <1827946+francoromito@users.noreply.github.com>
2020-05-16 21:55:24 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,13 +3,13 @@
 # devi fare il setup....
 
 date_default_timezone_set('UTC');
-define("YSFREFLECTORLOGPATH", "/var/log/YSFReflector");
-define("YSFREFLECTORLOGPREFIX", "log");
-define("YSFREFLECTORINIPATH", "/etc");
-define("YSFREFLECTORINIFILENAME", "YSFReflector.ini");
-define("YSFREFLECTORPATH", "/usr/local/bin/");
+define("YSFREFLECTORLOGPATH", "/var/log/YSFReflector"); // in base alle impostazioni di YSFReflecotr (processo di sistema)
+define("YSFREFLECTORLOGPREFIX", "log"); // prefisso del log file.. vedi il tuo YSFReflector.ini 
+define("YSFREFLECTORINIPATH", "/etc"); // sta qui ?
+define("YSFREFLECTORINIFILENAME", "YSFReflector.ini"); // è questo?
+define("YSFREFLECTORPATH", "/usr/local/bin/"); // sta qui ?
 define("TIMEZONE", "Europe/Rome");
-define("LOGO", "IT-DMR_NET.png");
+define("LOGO", "IT-DMR_NET.png"); // 
 define("REFRESHAFTER", "60");
 define("SHOWPROGRESSBARS", "on");
 ?>
