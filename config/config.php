fdaa84fd5778b5c2af1edd422cdb696415ac4561
Franco Romito <franco.romito@gmail.com>
2020-05-16 19:43:38 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+# This is an auto-generated config-file!
+# Be careful, when manual editing this!
+
+date_default_timezone_set('UTC');
+define("YSFREFLECTORLOGPATH", "/var/log/YSFReflector");
+define("YSFREFLECTORLOGPREFIX", "log");
+define("YSFREFLECTORINIPATH", "/etc");
+define("YSFREFLECTORINIFILENAME", "YSFReflector.ini");
+define("YSFREFLECTORPATH", "/usr/local/bin/");
+define("TIMEZONE", "Europe/Rome");
+define("LOGO", "IT-DMR_NET.png");
+define("REFRESHAFTER", "60");
+define("SHOWPROGRESSBARS", "on");
+define("TEMPERATUREALERT", "off");
+define("TEMPERATUREHIGHLEVEL", "60");
+define("SHOWQRZ", "off");
+define("GDPR", "0");
+?>
