267b910a6e49cab88e063085d1776ea9f178c90a
Franco Romito <franco.romito@gmail.com>
2020-05-16 19:48:43 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-# This is an auto-generated config-file!
-# Be careful, when manual editing this!
+
+# devi fare il setup....
 
 date_default_timezone_set('UTC');
 define("YSFREFLECTORLOGPATH", "/var/log/YSFReflector");
